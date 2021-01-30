6ae5b8cb0771449a24276b3fe1ccd57bb75080f5
Larry Shields <larry.shields@gmail.com>
2021-01-30 14:51:27 -0500

    Added Player Helper

---
+++ playerHelper.js
@@ -0,0 +1,23 @@
+const client = require('axios');
+const htmlHelper = require('./htmlHelper.js');
+const cheerio = require('cheerio');
+
+
+async function getPlayerName(playerUrl) {
+  var { data } = await client.get(playerUrl);
+
+  var nameHtml = htmlHelper.getHtml(data, 'h1.PlayerHeader__Name');
+  nameHtml = '<div>' + nameHtml + '</div>';
+  const $ = cheerio.load(nameHtml, {xmlMode: true});
+
+  var player = '';
+
+  $('span.min-w-0').each((idx, element) => {
+    var spanValue = $(element).text();
+
+    player += spanValue + ' ';
+  });
+  return player.trim();
+}
+
+exports.getPlayerName = getPlayerName;
