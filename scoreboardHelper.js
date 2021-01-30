b3d8f29431a5e7884a7d59f5805d0a7918cc0b4b
Larry Shields <larry.shields@gmail.com>
2021-01-30 14:51:36 -0500

    Updated Scoreboard Helper

--- scoreboardHelper.js
+++ scoreboardHelper.js
@@ -3,24 +3,27 @@ const client = require('axios');
 async function loadScoreData(dateValue, callback) {
   const { data } = await client.get('https://www.espn.com/nba/scoreboard/_/date/' + dateValue);
 
+
   const { JSDOM, VirtualConsole } = require('jsdom');
   const virtualConsole = new VirtualConsole();
 
   const dom = new JSDOM(data, {
     virtualConsole,
-    runScripts: "dangerously"  
+    runScripts: "dangerously"
 
   });
 
   dom.window.onload = () => {
+
     var scoreData = dom.window.espn.scoreboardData;
     var events = scoreData.events;
     var i;
     var games = [];
 
+
     for (i = 0; i < events.length; i++) {
-      
-      var item = events[i];      
+
+      var item = events[i];
       var competition = item.competitions[0];
       var competitors = competition.competitors;
       var game = {};
@@ -30,7 +33,7 @@ async function loadScoreData(dateValue, callback) {
       var j;
       var teams = [];
       for (j = 0; j < competitors.length; j++) {
-        var competitor = competitors[j];        
+        var competitor = competitors[j];
         var competitorTeam = competitor.team;
         var team = {};
         team.name = competitorTeam.displayName;
@@ -39,23 +42,25 @@ async function loadScoreData(dateValue, callback) {
 
         var lineScores = [];
         var scores = competitor.linescores;
-        
-        var k;
-        for (k = 0; k < scores.length; k++) {
-          var lineItem = scores[k];
-          var line = {};          
-          line.quarter = k + 1;
-          line.score = lineItem.value;
-          lineScores.push(line);         
-          
-        }                
-        team.lineScores = lineScores;            
+
+        if (scores) {
+          var k;
+          for (k = 0; k < scores.length; k++) {
+            var lineItem = scores[k];
+            var line = {};
+            line.quarter = k + 1;
+            line.score = lineItem.value;
+            lineScores.push(line);
+
+          }
+        }
+        team.lineScores = lineScores;
         teams.push(team);
       }
       game.date = item.date;
       game.teams = teams;
       games.push(game);
-      
+
     }
     callback(games);
   };
