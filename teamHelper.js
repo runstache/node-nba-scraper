75087f627f77814376b82034b6550d32eab0b056
Larry Shields <larry.shields@gmail.com>
2021-01-30 14:51:47 -0500

    Updated Team Helper

--- teamHelper.js
+++ teamHelper.js
@@ -3,44 +3,54 @@ const htmlHelper = require('./htmlHelper.js');
 const cheerio = require('cheerio');
 
 async function loadTeamStats(gameId) {
+  console.log('GETTING TEAM STATS FOR ' + gameId);
   var { data } = await client.get('https://www.espn.com/nba/matchup?gameId=' + gameId);
   var teamStatsHtml = htmlHelper.getHtml(data, '#teamstats-wrap');
   return teamStatsHtml;
 }
 
-function buildTeamTotals(teamHtml) {
+function buildTeamTotals(teamHtml, id) {
   var teams = [];
   var homeTeam = {};
   var awayTeam = {};
 
-  var statHtml = htmlHelper.getHtml(teamHtml, 'tbody');
+  try {
+    var statHtml = htmlHelper.getHtml(teamHtml, 'tbody');
 
-  const $ = cheerio.load(statHtml, {
-    xmlMode: true
-  });
-  homeTeam.type = 'home';
-  homeTeam.stats = [];
-  awayTeam.type = 'away';
-  awayTeam.stats = [];
+    if (statHtml) {
+      const $ = cheerio.load(statHtml, {
+        xmlMode: true
+      });
+      homeTeam.type = 'home';
+      homeTeam.stats = [];
+      awayTeam.type = 'away';
+      awayTeam.stats = [];
 
-  $('tr').each(function(idx, el) {
-    var statRow = $(this).html();
-    var statName = htmlHelper.getValue(statRow, 'td', 0);
-    var awayStat = htmlHelper.getValue(statRow, 'td', 1);
-    var homeStat = htmlHelper.getValue(statRow, 'td', 2);
+      $('tr').each(function (idx, el) {
+        var statRow = $(el).html();
+        var statName = htmlHelper.getValue(statRow, 'td', 0);
+        var awayStat = htmlHelper.getValue(statRow, 'td', 1);
+        var homeStat = htmlHelper.getValue(statRow, 'td', 2);
 
-    var homeStatObject = {};
-    homeStatObject.type = statName.replace(/[\s\r\n\t]/g, '');
-    homeStatObject.value = homeStat.replace(/[\s\r\n\t]/g, '');
-    homeTeam.stats.push(homeStatObject);
+        var homeStatObject = {};
+        homeStatObject.type = statName.replace(/[\s\r\n\t]/g, '');
+        homeStatObject.value = homeStat.replace(/[\s\r\n\t]/g, '');
+        homeTeam.stats.push(homeStatObject);
 
-    var awayStatObject = {};
-    awayStatObject.type = statName.replace(/[\s\r\n\t]/g, '');
-    awayStatObject.value = awayStat.replace(/[\s\r\n\t]/g, '');
-    awayTeam.stats.push(awayStatObject);
-  });
-  teams.push(homeTeam);
-  teams.push(awayTeam);
+        var awayStatObject = {};
+        awayStatObject.type = statName.replace(/[\s\r\n\t]/g, '');
+        awayStatObject.value = awayStat.replace(/[\s\r\n\t]/g, '');
+        awayTeam.stats.push(awayStatObject);
+      });
+      teams.push(homeTeam);
+      teams.push(awayTeam);
+    } else {
+      console.log('NO TEAM HTML FOUND');
+    }
+  } catch (err) {
+    console.log('FAILED TO FIND TEAM STATS FOR GAME:' + id + ':' + err);
+  }
+
   return teams;
 }
 
